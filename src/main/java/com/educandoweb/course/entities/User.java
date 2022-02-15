5d5fdc6064b56cf59ccf842810a89980f519846c
Ivo Aguiar <ivoaguiarpimenta@gmail.com>
2022-02-15 10:51:07 -0300

    User insert

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -22,7 +22,7 @@ public class User implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	private String name;
 	private String email;
 	private String phone;
 	private String password;
@@ -40,7 +40,7 @@ public class User implements Serializable{
 	public User(Long id, String nome, String email, String phone, String password) {
 		super();
 		this.id = id;
-		this.nome = nome;
+		this.name = nome;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
@@ -57,13 +57,13 @@ public class User implements Serializable{
 	}
 
 
-	public String getNome() {
-		return nome;
+	public String getName() {
+		return name;
 	}
 
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 
